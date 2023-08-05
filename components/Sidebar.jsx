50baa39d318381be02c6d45ea1f6488f28f00ee9
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-08-04 23:22:51 -0600

    Agregando imagenes

---
+++ components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import Image from "next/future/image"
+const Sidebar = () => {  
+  return (
+    <>
+        <Image src={"/assets/img/logo.jpg"} alt="imagen logo" width={250} height={100}/>
+    </>
+  )
+}
+
+export default Sidebar
--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,18 +1,18 @@
 import Head from "next/head";
-
+import Sidebar from "../components/Sidebar";
 
 const Layout = ({children,pagina}) => {
   return (
     <>
         <Head>
-            <title>Taqueria - {pagina}</title>
+            <title>{`Taqueria - ${pagina}`}</title>
             <meta name="description" content="taqueria las vecinas"/>
         </Head>
         <div className=" md:flex">
-            <aside className=" bg-red-500 md:w-1/4">
-                Sidebar aqui
+            <aside className="   md:w-4/12 xl:w-1/4 2xl:w-1/5">
+                <Sidebar/>
             </aside>
-            <main className=" bg-blue-500 md:w-3/4 md:h-screen md:overflow-y-scroll">
+            <main className=" md:w-8/12 xl:w-3/4 2xl:w-4/5 md:h-screen md:overflow-y-scroll">
                 {children}
             </main>
         </div>
